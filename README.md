6284ee5cdf4c7f42a862e302d3ffe240d2509d6c
e2718281828 <mardiev.1999@gmail.com>
2023-05-09 14:23:56 +0300

    Дополнил readme

--- README.md
+++ README.md
@@ -4,4 +4,14 @@
 
 3. yarn run server - запустить json-server
 
-Таблица находится на http://localhost:3000/employees
+Таблица с сотрудниками находится на http://localhost:3000/employees
+
+Родительский компонент находится в page/employees
+
+UI-компоненты - components/ui, для них настроен автоимпорт
+
+Компоненты, связанные с таблицей сотрудников - components/common/employees
+
+Запросы - api/employees.js
+
+Данные - data/db.json
--- api/employees.js
+++ api/employees.js
@@ -1,19 +1,21 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default async function getEmployees() {
-  try {
-    const response = await axios.get("http://localhost:3100/employees");
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await axios.get("http://localhost:3100/employees");
+  return response.data;
 }
 export const createEmployee = async (employee) => {
-  const response = await axios.post('http://localhost:3100/employees', employee)
-  return response.data
-}
+  const response = await axios.post(
+    "http://localhost:3100/employees",
+    employee
+  );
+  return response.data;
+};
 
 export const updateEmployee = async (id, employee) => {
-  const response = await axios.put(`http://localhost:3100/employees/${id}`, employee)
-  return response.data
-}
+  const response = await axios.put(
+    `http://localhost:3100/employees/${id}`,
+    employee
+  );
+  return response.data;
+};
